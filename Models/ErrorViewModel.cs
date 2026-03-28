998d4decf3f5bfc44ee63fb971dd4c5a1d430192
Filipe Lima Guedes chagas <filipechgs@gmail.com>
2026-03-28 16:59:55 -0300

    inicia versionamento

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCDotNetApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
